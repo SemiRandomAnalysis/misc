553f4d62195b491429c3971a2a8a54ae2b562f0f
SemiRandomAnalysis <23327537+SemiRandomAnalysis@users.noreply.github.com>
2017-11-19 23:56:49 -0800

    a

--- Projects/ergast/laptime_record_validation.sql
+++ Projects/ergast/laptime_record_validation.sql
@@ -12,7 +12,7 @@ select *
         AND isnull(l.position)
 	order by races.date;
 
-# Check if the highest lap's laptime matches the number of completed laps for a driver        
+# Check if a laptime record for the driver's recorded laps lap exists
 select *
 	from races
     join results r on r.raceId = races.raceId
@@ -26,32 +26,35 @@ select *
 	order by races.date;
 
 # check if the # of laptime records in a race for a driver matches his completed laps count
-select *,
-	sum(if(isnull(laptime_position),0,1)) as laptime_records,
-    laps - sum(if(isnull(laptime_position),0,1)) as delta_in_laptime_records_vs_laps
-   
+select *
 	from (
-	select races.year,
-		   races.date,
-           races.name,
-           races.raceId,
-           driverRef,
-           d.driverId,
-           r.grid,
-           r.position as position,
-#           l.lap as laptime_lap,
-           l.position as laptime_position,
-           r.laps
-           
-		from races
-		join results r on r.raceId = races.raceId
-		join drivers d on d.driverId = r.driverId
-		left join laptimes l on l.raceId = r.raceId
-			AND l.driverId = r.driverId
-			AND l.lap = r.laps
-		where races.year > 1996 
-		order by races.date
-	) query1
-    group by driverId, raceId
-	#order by delta_in_laptime_records_vs_laps desc
-    order by date, driverRef
+	select *,
+		sum(if(isnull(laptime_position),0,1)) as laptime_records,
+		laps - sum(if(isnull(laptime_position),0,1)) as delta_in_laptime_records_vs_laps
+	   
+		from (
+		select races.year,
+			   races.date,
+			   races.name,
+			   races.raceId,
+			   driverRef,
+			   d.driverId,
+			   r.grid,
+			   r.position as position,
+	#           l.lap as laptime_lap,
+			   l.position as laptime_position,
+			   r.laps
+			   
+			from races
+			join results r on r.raceId = races.raceId
+			join drivers d on d.driverId = r.driverId
+			left join laptimes l on l.raceId = r.raceId
+				AND l.driverId = r.driverId
+			where races.year > 1996 
+			order by races.date
+		) query1
+		group by driverId, raceId
+		#order by delta_in_laptime_records_vs_laps desc
+		order by date, driverRef
+	) query2
+    where laps <> laptime_records
